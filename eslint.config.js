6e531380a7e9e954604f1f9cc5184a0e0e27867e
Nour Eddin Al Shammari <noureddinalshammari@gmail.com>
2024-10-21 17:52:21 +0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,15 +4,23 @@ module.exports = [
     ignores: ["node_modules/**"],
     languageOptions: {
       ecmaVersion: "latest",
-      sourceType: "module"
+      sourceType: "module",
+      parser: require("@babel/eslint-parser"),  // Use Babel parser for JSX/ES6+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,   // Enable JSX
+        },
+        requireConfigFile: false,  // Allows parsing without a babel config file
+      },
     },
     plugins: {
-      react: require("eslint-plugin-react")
+      react: require("eslint-plugin-react"),
+      "react-hooks": require("eslint-plugin-react-hooks")
     },
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "no-console": "off",
-      "no-unused-vars": "warn"
+      "react/react-in-jsx-scope": "off",   // Not needed in React 17+
+      "no-console": "off",                 // Allow console statements
+      "no-unused-vars": "warn",             // Disable unused-vars rule
     }
   }
 ];
