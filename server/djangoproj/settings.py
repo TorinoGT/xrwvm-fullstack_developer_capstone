7342840a2235c8cffbbe717116b5ad51548ca353
TorinoGT <noureddinalshammari@gmail.com>
2024-10-22 10:30:04 -0400

    Finished deployment

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoproj.urls'
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,6 +4,6 @@
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python3 manage.py migrate --run-syncdb --noinput
+#python3 manage.py migrate --run-syncdb --noinput
 python manage.py collectstatic --noinput
 exec "$@"
