53bea0dd5c9da33ef3aa07297ecd788a253c0573
TorinoGT <noureddinalshammari@gmail.com>
2024-10-24 13:03:55 -0400

    Website enhancement part 1 done

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -6,8 +6,11 @@ import review_icon from "../assets/reviewicon.png"
 
 const Dealers = () => {
   const [dealersList, setDealersList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [originalDealers, setOriginalDealers] = useState([]);
   // let [state, setState] = useState("")
   let [states, setStates] = useState([])
+  
 
 let root_url = window.location.origin
 //   let dealer_url ="/djangoapp/get_dealers";
@@ -44,11 +47,27 @@ let root_url = window.location.origin
 
       setStates(Array.from(new Set(states)))
       setDealersList(all_dealers)
+      setOriginalDealers(all_dealers);
     }
   }
   useEffect(() => {
     get_dealers();
   },[]);  
+
+const handleInputChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    const filtered = originalDealers.filter(dealer =>
+    dealer.state.toLowerCase().includes(query.toLowerCase())
+    );
+    setDealersList(filtered);
+};
+
+const handleLostFocus = () => {
+if (!searchQuery) {
+  setDealersList(originalDealers);
+}
+}
 
 
 let isLoggedIn = sessionStorage.getItem("username") != null ? true : false;
@@ -64,13 +83,7 @@ return(
       <th>Address</th>
       <th>Zip</th>
       <th>
-      <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
-      <option value="" selected disabled hidden>State</option>
-      <option value="All">All States</option>
-      {states.map(state => (
-          <option value={state}>{state}</option>
-      ))}
-      </select>        
+      <input type="text" placeholder="Search states..." onChange={handleInputChange} onBlur={handleLostFocus} value={searchQuery} />      
 
       </th>
       {isLoggedIn ? (
